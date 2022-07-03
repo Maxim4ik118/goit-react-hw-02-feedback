8daf775695e51dc93e501d944b2ace597eef8ce5
Maxi <mmaximonchuk@gmail.com>
2022-07-03 18:41:38 +0300

    new: build

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
-export { default as Notification } from './Notification/Notification';
-export { default as Section } from './Section/Section';
-export { default as Statistics } from './Statistics/Statistics';
